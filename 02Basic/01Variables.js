6f8cf788a2e1d30527b06d59e506405d96e5fac3
Nirodha97 <nirodhamwickramaratne@gmail.com>
2022-07-02 12:22:47 +0530

    Hello & Variables

---
+++ 02Basic/01Variables.js
@@ -0,0 +1,14 @@
+var fullName = "Nirodha Wickramarathna";
+var courseName ="Javascript Tutorial";
+
+var isLoggedIn = false;
+
+var loggedCount =34;
+
+// console.log(loggedCount);
+// console.log("loggedCount");
+
+var paymentMode;
+
+paymentMode= "Credit Card";
+console.log(paymentMode);

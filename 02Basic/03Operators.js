2063b0190688fd5d1dbbf9ab0ac7e0ddc8c1545d
Nirodha97 <nirodhamwickramaratne@gmail.com>
2022-07-03 03:08:09 +0530

    Operators and Conditionals

---
+++ 02Basic/03Operators.js
@@ -0,0 +1,20 @@
+var num1=7;
+var num2=6;
+
+console.log(num1*num2);
+
+var answer= num1>num2;
+console.log(answer);
+console.log("Typeof: "+typeof answer);
+
+var sellingPrice = 199;
+var listingPrice=799;
+
+var discountPercent= ((listingPrice-sellingPrice)/listingPrice)*100;
+
+console.log("Discont : "+discountPercent);
+
+displayDiscountPercentage = Math.round(discountPercent);
+console.log(displayDiscountPercentage);
+
+
---
+++ 02Basic/04Conditionals.js
@@ -0,0 +1,14 @@
+var temperature;
+
+temperature=40;
+
+
+if(temperature<20){
+    console.log("Very Cold");
+}
+
+if(temperature<30){
+    console.log("It's Moderate");
+}else{
+    console.log("It's Really Hot");
+}
